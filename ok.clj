d6b59550891913df3795b93479690e5d4c208b76
Nicolas Modrzyk <hellonico@gmail.com>
2019-09-03 13:03:52 +0900

    test repository

--- ok.clj
+++ ok.clj
@@ -1,5 +1,6 @@
 #!/usr/bin/env inlein
-'{:dependencies [[origami/origami "4.1.1-3"]]}
+'{:repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
+  :dependencies [[origami/origami "4.1.1-3"]]}
 
 (require '[opencv4.core :refer [VERSION]]) 
 (println "Using OpenCV Version: " VERSION "...")
