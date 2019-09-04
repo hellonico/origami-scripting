b3633d585ae29bf76a80fbab2a4e32435eae9504
Nicolas Modrzyk <hellonico@gmail.com>
2019-09-04 18:21:07 +0900

    dnn yeah

---
+++ dnn_on_cam.clj
@@ -0,0 +1,15 @@
+#!/usr/bin/env inlein
+
+'{:dependencies [[origami-dnn "0.1.0"]]}
+
+(require '[origami-dnn.net.mobilenet :refer [find-objects]]
+          '[origami-dnn.core :refer [read-net-from-uri]]
+          '[origami-dnn.draw :as d]
+          '[opencv4.utils :refer [simple-cam-window]])
+
+(let [ [net opts labels] (read-net-from-uri "http://repository.hellonico.info/repository/hellonico/origami-dnn-networks/mobilenet/1.0.0/mobilenet-1.0.0.zip") ]
+  (simple-cam-window
+   (fn [buffer]
+     (-> buffer 
+      (find-objects net opts) 
+      (d/red-boxes! labels)))))
--- ok.clj
+++ ok.clj
@@ -1,6 +1,5 @@
 #!/usr/bin/env inlein
-'{:repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
-  :dependencies [[origami/origami "4.1.1-4"]]}
+'{:dependencies [[origami/origami "4.1.1-4"]]}
 
 (require '[opencv4.core :refer [VERSION]]) 
 (println "Using OpenCV Version: " VERSION "...")
