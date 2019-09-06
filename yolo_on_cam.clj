d580a9f4ec4a0297bc7eaf1be07e2b686bf16613
Nicolas Modrzyk <hellonico@gmail.com>
2019-09-06 15:36:58 +0900

    yolo scripted

---
+++ yolo_on_cam.clj
@@ -0,0 +1,18 @@
+#!/usr/bin/env inlein
+
+'{:dependencies [[origami-dnn "0.1.1-SNAPSHOT"][origami "4.1.1-5-SNAPSHOT"]]}
+
+(ns demo.yolo.cam
+  (:require   [origami-dnn.net.yolo :as yolo]
+                     [origami-dnn.draw :as d]
+                     [origami-dnn.core :as origami-dnn]
+                     [opencv4.utils :as u]))
+
+   (let [[net _ labels] (origami-dnn/read-net-from-repo "networks.yolo:yolov3-tiny:1.0.0")]
+			  (u/simple-cam-window
+			  {:frame {:fps true}}
+			   (fn [buffer]
+			     (-> buffer 
+						     (u/resize-by 0.5)
+						     (yolo/find-objects net) 
+						     (d/blue-boxes! labels) ))))
