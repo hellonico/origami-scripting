54c0a3adec05620fc706e5104e5cb387bf98f5dd
Nicolas Modrzyk <hellonico@gmail.com>
2020-04-24 09:26:52 +0900

    date compare

--- timelapse_make.clj
+++ timelapse_make.clj
@@ -6,11 +6,11 @@
 (require '[progrock.core :as pr])
 
 (def output-file (or (first *command-line-args*)  "timelapse.mp4"))
-(def input (or (first *command-line-args*)  "out"))
+(def input (or (second *command-line-args*)  "out"))
+(def start-time  (.getEpochSecond (java.time.Instant/parse "2020-04-22T23:55:30.00Z")))
 
 (def files  
   (sort-by #(.lastModified %) (file-seq (clojure.java.io/as-file input))))
-
 
 (def  fourcc 1196444237)
 (def w (cvv/new-videowriter))
@@ -23,10 +23,11 @@
 (loop [files files bar (pr/progress-bar (count files))]
 (if (empty? files) 
 (do (.release w) (pr/print (pr/done bar)))
-(let [path (.getPath (first files)) img (cv/imread path)]
-	(.write w img)
+(let [file (first files) lm (.lastModified file)]
+ (if (> lm start-time)
+ (let [img (cv/imread (.getPath file)) ]
+		(.write w img) (.release img)))
 	(pr/print bar)
-	(.release img)
 	(recur (rest files) (pr/tick bar)))))
 
 (println "Video Finished > " (java.util.Date.))
